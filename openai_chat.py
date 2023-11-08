8b866733daaf2a9b9b3a91511300174323b5ef71
Brady Rhora <brady0423@gmail.com>
2023-11-07 21:28:53 -0500

    auto model switch fix

--- openai_chat.py
+++ openai_chat.py
@@ -10,33 +10,42 @@ class Chat:
 
     def send_message(self, user, message, image_url):
         if image_url:
-            model="gpt-4-vision-preview"
-            self.message_history.append(ImageChatMessage('user', user, message, image_url))
+            self.message_history.append(ChatMessage('user', user, message, image_url))
         else:
-            model="gpt-3.5-turbo"
             self.message_history.append(ChatMessage('user', user, message))
 
+            
         messages = list(map(lambda m: m.getJSON(), self.message_history[-MESSAGE_HISTORY_LENGTH:]))
         messages.insert(0, ChatMessage('system', 'system', SYSTEM_MESSAGE).getJSON())
+
+        if any(map(lambda m: len(m['content']) > 1, messages)):
+            model="gpt-4-vision-preview"
+        else:
+            model="gpt-3.5-turbo"
 
         response = self.client.chat.completions.create(
             model=model,
             messages=messages,
             max_tokens=256,
         )
+
+
         print("[RESPONSE DEBUG INFO]")
         print("Model used: " + response.model)
-        print("Finish reason: " + response.choices[0].finish_reason)
+        if len(response.choices) > 0 and response.choices[0].finish_reason is not None:
+            print("Finish reason: " + response.choices[0].finish_reason)
 
         self.message_history.append(ChatMessage('assistant', 'Stevey', response.choices[0].message.content))
         return response.choices[0].message.content
 
 class ChatMessage:
-    def __init__(self, role, user, content):
+    def __init__(self, role, user, content, image_url=None):
         self.role = role
         self.user = user
         msg = user +": " + content if role == "user" else content
         self.content = [{"type": "text", "text": msg}]
+        if image_url:
+            self.content.append({"type":"image_url", "image_url": image_url})
 
     def getJSON(self):
         return {
@@ -47,8 +56,3 @@ class ChatMessage:
 class BotMessage(ChatMessage):
     def __init__(self, content):
         super().__init__("assistant", "Stevey", content)
-
-class ImageChatMessage(ChatMessage):
-    def __init__(self, role, user, content, image_url):
-        super().__init__(role, user, content)
-        self.content.append({"type":"image_url", "image_url": image_url})
